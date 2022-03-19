78b354de552e2418c58612b6844e1612a205c64d
M. Fadli Zein <45892408+gzeinnumer@users.noreply.github.com>
2022-03-19 14:25:19 +0700

    Initial commit

---
+++ SwitfUI T22/SwitfUI_T22App.swift
@@ -0,0 +1,17 @@
+//
+//  SwitfUI_T22App.swift
+//  SwitfUI T22
+//
+//  Created by M Fadli Zein on 19/03/22.
+//
+
+import SwiftUI
+
+@main
+struct SwitfUI_T22App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
